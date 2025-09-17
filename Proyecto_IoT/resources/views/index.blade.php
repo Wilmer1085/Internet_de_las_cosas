5ec5f783fe248710ac0db66a53bb007647d915d0
wilmer Bermudes <wilmer1085@hotmail.com>
2025-09-16 20:17:28 -0500

    Nuevo archivo proyecto IoT

--- Proyecto_IoT/resources/views/index.blade.php
+++ Proyecto_IoT/resources/views/index.blade.php
@@ -2,28 +2,11 @@
 @section('title','inicio')
 @section('content')
 
-<div class="container mt-4">
-
-<!-- Título de bienvenida -->
-    <h1 class="mb-4 text-center">Bienvenido al portal de Proyectos IoT</h1>
-
 <div class="card" style="width: 18rem;">
-  <img src="/img/img IoT.jpg" class="card-img-top" alt="Dispositivo IoT">
+  <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-    <p class="card-text">Explora proyectos y soluciones innovadoras con dispositivos conectados al Internet de las Cosas (IoT).</p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
   </div>
-</div>
-
-
-<!-- Sección de botones -->
-    <div class="d-flex flex-column flex-md-row justify-content-center gap-3 mb-5">
-        <button class="btn btn-primary">Tablas</button>
-        <button class="btn btn-success">Gráficas</button>
-        <button class="btn btn-warning">Formulario</button>
-        <button class="btn btn-info">Registrar Datos</button>
-    </div>
-
-
 </div>
 
 @endsection
