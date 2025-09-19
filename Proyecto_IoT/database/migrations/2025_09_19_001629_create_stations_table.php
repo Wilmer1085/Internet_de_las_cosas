b944d6bcba734734d8d37f093de56bb864651537
wilmer1085 <wilmer1085@hotmail.com>
2025-09-18 19:53:29 -0500

    configuracion bases de datos

---
+++ Proyecto_IoT/database/migrations/2025_09_19_001629_create_stations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('stations', function (Blueprint $t) {
+            $t->id();
+            $t->string("name");
+            $t->string("code")->nullable();
+            $t->boolean("status")->default(true);
+            $t->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+};
