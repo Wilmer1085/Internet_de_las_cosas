e55680034dd7bcc29993d141d1b7315cf136f6d4
wilmer1085 <wilmer1085@hotmail.com>
2025-10-02 19:51:31 -0500

    cracion de controladores y apis

---
+++ Proyecto_IoT/app/Http/Controllers/sensorsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sensors;
+use Illuminate\Http\Request;
+
+class sensorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\sensors  $sensors
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sensors $sensors)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\sensors  $sensors
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sensors $sensors)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\sensors  $sensors
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sensors $sensors)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\sensors  $sensors
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sensors $sensors)
+    {
+        //
+    }
+}
